fae7ef3bef81b11efaf2706ad905d74781bc6d80
Pedro Solozabal <pedrosolozabal@gmail.com>
2024-10-17 19:32:30 -0300

    Refatorando: JpaConfig, application.properties, schema.sql e PixAutomaticoApplicationTests.

---
+++ pix-automatico/src/main/java/com/solozabal/pixautomatico/pix_automatico/config/JpaConfig.java
@@ -0,0 +1,31 @@
+package com.solozabal.pixautomatico.pix_automatico.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.solozabal.pixautomatico.pix_automatico.entity");
+        // Configurações adicionais
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+}
